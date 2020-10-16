27188c301e6bb573799fef0c5d3243eddcb1af1e
Vivek Joy <vivekjne@gmail.com>
2020-10-16 12:48:27 +0530

    first commit

---
+++ day1/lib/app.js
@@ -0,0 +1,11 @@
+var aa = [];
+aa.push(1);
+console.log(aa);
+var tuple1 = [
+    123,
+    '123',
+    'Good morning',
+    1010
+];
+console.log({ tuple1: tuple1 });
+//# sourceMappingURL=app.js.map
